af06d22f7c54b2435bb6dce9e031e70687c4ce5d
TongDangTinh <tinhtongdang@gmail.com>
2021-11-01 20:30:28 +0700

    bai 2

--- Day1/tinh.py
+++ Day1/tinh.py
@@ -1,6 +1,10 @@
 sum_number = 0
+sum_1 = 0
+
 for i in range(0, 1000):
-    if(i%2 != 1):
-    
-        sum_number =+ i
-print(sum_number)
+    if(i%3 == 0 and i%3 == 5):
+        sum_number =+ i 
+    else:
+        sum_1 =+ i
+
+print(sum_number - sum_1)
