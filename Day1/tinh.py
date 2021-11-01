8eb59498d1594805cc115e9bb8dea8889ad8676c
TongDangTinh <tinhtongdang@gmail.com>
2021-11-01 20:50:35 +0700

    Tim so nguyen to

--- Day1/tinh.py
+++ Day1/tinh.py
@@ -1,10 +1,17 @@
-sum_number = 0
-sum_1 = 0
-
-for i in range(0, 1000):
-    if(i%3 == 0 and i%3 == 5):
-        sum_number =+ i 
-    else:
-        sum_1 =+ i
-
-print(sum_number - sum_1)
+n = int(input())
+prime_number = True
+if (n < 2):
+    prime_number = False
+elif (n == 2):
+    prime_number = True
+elif (n % 2 == 0):
+    prime_number = False
+else:
+    for i in range(3, n, 2):
+        if (n % i == 0):
+            prime_number = False
+ 
+if prime_number == True:
+    print(n, " là số nguyên tố")
+else:
+    print(n, " không phải là số nguyên tố")
