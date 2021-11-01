6a406280963cc3bc5de30d18478fc9f3ca150e9f
Thong Tran <thongrun2s33@gmail.com>
2021-11-01 20:17:03 +0700

    tổng chia hêt  3 5

--- Day1/thong.py
+++ Day1/thong.py
@@ -2,3 +2,11 @@ tổng_số_lẻ = 0
 for i in range(1, 1000, 2):
     tổng_số_lẻ += i
 print(tổng_số_lẻ)
+
+
+tổng_chia_hết_3_5 = 0
+for i in range(1, 1000):
+    if i % 3 == 0 and i % 5 == 0:
+        tổng_chia_hết_3_5 += i
+
+print(tổng_chia_hết_3_5)
