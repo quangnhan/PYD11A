cbf2f4dcf2177a4e0d9f901436363a66bd672533
Thong Tran <thongrun2s33@gmail.com>
2021-11-03 20:18:14 +0700

    comment

--- Day2/thong.py
+++ Day2/thong.py
@@ -1,8 +1,16 @@
 matrix1 = [[2, 3, 4], [5, 6, 7], [8, 9, 10]]
 
-matrix2 = [["a", "b", "c"], ["a", "b", "c"], ["a", "b", "c"]]
+matrix2 = [[8, 9, 10], [11, 12, 13], [14, 15, 16]]
 
-for x in matrix1:
-    new_matrix = [item + x for item in matrix2]
+matrix1_list = []
+for list in matrix1:
+    for x in list:
+        matrix1_list.append(x)
+print(matrix1_list)
 
-print(new_matrix)
+for list in matrix2:
+    for x in list:
+        matrix3 = [x + num for num in matrix1_list]
+
+
+print(matrix3)
