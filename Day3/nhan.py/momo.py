773ad04261fc7a808fc51dd86b550cc448a29492
quangnhan145 <nhan@leadplus.net>
2021-11-08 20:34:39 +0700

    Add excercise 3 day 3

--- Day3/nhan.py/momo.py
+++ Day3/nhan.py/momo.py
@@ -2,15 +2,17 @@ from server import Server
 
 class Momo:
     def __init__(self, email):
-        server = Server()
-        
+        self.server = Server()
         self.email = email
-        self.full_name = server.get_full_name(email)
+        self.full_name = self.server.get_full_name(email)
 
-        print(f"Hello {self.full_name}")
+    def get_amount(self):
+        return self.server.get_amount(self.email)
 
 if __name__ == "__main__":
     momo = Momo("nhan@gmail.com")
+    amount = momo.get_amount()
+    print(amount)
     
 
 
--- Day3/nhan.py/server.py
+++ Day3/nhan.py/server.py
@@ -1,28 +1,50 @@
+from datetime import datetime
+
 class Server:
     def __init__(self, name="No name"):
         self.name = name
-        self.CUSTOMERS = {
+        self.__CUSTOMERS = {
             "thong@gmail.com": {
                 "phone": "1111111111",
                 "full_name": "Vo Quang Nhan 1",
+                "amount": "100"
             },
             "tinh@gmail.com": {
                 "phone": "2222222222",
                 "full_name": "Vo Quang Nhan 2",
+                "amount": "200"
             },
             "nhan@gmail.com": {
                 "phone": "333333333",
                 "full_name": "Vo Quang Nhan 3",
+                "amount": "300"
             },
         }
+        self.__log = list()
+
+    def __set_log(self, message):
+        self.__log.append(message)
+
+    def get_log(self):
+        print(self.__log)
 
     def get_full_name(self, email):
-        if self.CUSTOMERS.get(email):
-            return self.CUSTOMERS[email]["full_name"]
+        self.__set_log(f"{email} get name at {str(datetime.now())}")
+
+        if self.__CUSTOMERS.get(email):
+            return self.__CUSTOMERS[email]["full_name"]
         return "Email doesn't exiest"
 
+    def get_amount(self, email):
+        self.__set_log(f"{email} get amount at {str(datetime.now())}")
+
+        if self.__CUSTOMERS.get(email):
+            return self.__CUSTOMERS[email]["amount"]
+        return 0
 
 if __name__ == "__main__":
     server = Server("Mock Server")
     full_name = server.get_full_name("thong@gmail.com")
-    print(full_name)
+    full_name = server.get_full_name("thong@gmail.com")
+    full_name = server.get_full_name("thong@gmail.com")
+    server.get_log()
