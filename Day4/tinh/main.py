06407fb7e5afa61b3538f931c67aea15735c8beb
TongDangTinh <tinhtongdang@gmail.com>
2021-11-15 20:20:55 +0700

    Add more

--- Day4/tinh/main.py
+++ Day4/tinh/main.py
@@ -1,23 +1,44 @@
-class Human:
+from momo_server import MoMoServer
+from vnpay_server import VNPayServer
+from zalopay_server import ZaloPayServer
+
+class PaymentApp:
+    amount = 0
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-class MoMo(Human):
-    def pay(self, mount_momo):
-        self.mount_momo = mount_momo 
+
+    def pay(self, amount):
+        if self.amount >= amount:
+            self.amount -= amount
+            print(f"So du {amount}")
+        else:
+            print("So du cua ban khong du")
+
+class Momo(PaymentApp):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        server = MoMoServer()
+        self.amount = float(server.get_amount(username, password))
+
+class VNPay(PaymentApp):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        server = VNPayServer()
+        self.amount = float(server.get_amount(username, password))
 
 
-class VNPay(Human):
-    def pay(self, mount_vnpay):
-        self.mount_vnpay = mount_vnpay
+class Zalo_Pay(PaymentApp):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        server = ZaloPayServer()
+        self.amount = float(server.get_amount(username, password))
 
-class ZaloPay(Human):
-    def pay(self, mount_zalopay):
-        self.mount_zalopay = mount_zalopay
+user = Momo("username 2", "password 2")
+print(user.amount)
+user = VNPay("username 2", "password 2")
+print(user.amount)
 
-
-zalo = ZaloPay("Tinh","01929")
-print(zalo)
-
-print(zalo.pay(100))
+user = Zalo_Pay("username 2", "password 2")
+print(f"so du {user.amount}")
