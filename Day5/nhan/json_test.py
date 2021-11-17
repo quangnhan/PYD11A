28e3b3c8e47e380ee87c6b9d4df7605ea7fea6f8
quangnhan145 <nhan@leadplus.net>
2021-11-17 19:19:31 +0700

    Add new file json

--- Day5/nhan/json_test.py
+++ Day5/nhan/json_test.py
@@ -1,11 +1,22 @@
 import os
 import json
 
-file_path = f"{os.getcwd()}/Day5/data.json"
-f = open(file_path)
+# file_path = f"{os.getcwd()}/Day5/data.json"
+# f = open(file_path)
 
-list_data = f.read()
-list_data = json.loads(list_data)
+# list_data = f.read()
+# list_data = json.loads(list_data)
 
-for data in list_data:
-    print(data)
+list_data = [
+    {
+        "id": 1,
+        "name": "Name 1"
+    },
+    {
+        "id": 2,
+        "name": "Name 3"
+    }
+]
+new_file = f"{os.getcwd()}/Day5/nhan/new_data.json"
+f = open(new_file, "w")
+json.dump(list_data, f)
