b18e910ff2e3e14f674d6ac3f3574cd747de4a3a
quangnhan145 <nhan@leadplus.net>
2021-11-17 19:49:30 +0700

    Add header style

--- Day5/nhan/excel_test.py
+++ Day5/nhan/excel_test.py
@@ -7,12 +7,28 @@ df = pd.DataFrame({
     'Age': [101, 201, 301],
 })
 
-# # Create a Pandas Excel writer using XlsxWriter as the engine.
+# Create a Pandas Excel writer using XlsxWriter as the engine.
 path = f"{os.getcwd()}/Day5/nhan/pandas_simple.xlsx"
 writer = pd.ExcelWriter(path, engine='xlsxwriter')
 
-# # Convert the dataframe to an XlsxWriter Excel object.
-df.to_excel(writer, sheet_name='Nathan')
+# Turn off the default header and skip one row to allow us to insert a
+# user defined header.
+df.to_excel(writer, sheet_name='Sheet1', startrow=1, header=False, index=False)
 
-# # Close the Pandas Excel writer and output the Excel file.
+# Get the xlsxwriter workbook and worksheet objects.
+workbook  = writer.book
+worksheet = writer.sheets['Sheet1']
+
+# Add a header format.
+header_format = workbook.add_format({
+    'bold': True,
+    'text_wrap': True,
+    'valign': 'top',
+    'fg_color': '#D7E4BC',
+    'border': 1})
+
+# Write the column headers with the defined format.
+for col_num, value in enumerate(df.columns.values):
+    worksheet.write(0, col_num, value, header_format)
+
 writer.save()
