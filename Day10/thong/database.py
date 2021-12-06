04c6ca72aa7a29d759f07b7c1cf0c1770bba9395
Thong Tran <thongrun2s33@gmail.com>
2021-12-06 19:30:31 +0700

    add changes to databases.py

--- Day10/thong/database.py
+++ Day10/thong/database.py
@@ -1,19 +1,24 @@
-import pymysql
+import psycopg2
+
 
 class Database:
     def __init__(self):
-        self.connection = pymysql.connect(host='127.0.0.1',
-                             user='root',
-                             password='123456',                             
-                             db='shop',) 
-        self.cur = self.connection.cursor()
+
+        self.conn = psycopg2.connect(
+            database="day10",
+            user="postgres",
+            password="Abcd@1234",
+            host="127.0.0.1",
+            port="5432",
+        )
+        self.cur = self.conn.cursor()
 
     def get_all_user(self):
-        self.cur.execute(f"SELECT * FROM user")
+        self.cur.execute(f"SELECT * FROM users")
         data = self.cur.fetchall()
         return data
 
     def get_all_product(self):
-        self.cur.execute(f"SELECT * FROM user")
+        self.cur.execute(f"SELECT * FROM users")
         data = self.cur.fetchall()
         return data
