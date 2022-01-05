af2b2e4799dfd8abf71dca0d3d2221d3f763893d
nhanvo <nhan.vo@pricefx.eu>
2022-01-05 20:36:24 +0700

    rend view models

--- Day16/duc-nhan/customers/views.py
+++ Day16/duc-nhan/customers/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Customer
 
 # Create your views here.
+def customerList(request):
+    listCustomers = Customer.objects.all()
+    data = {
+        "customers": listCustomers
+    }
+    return render(request, "customers/customer_list.html", data)
