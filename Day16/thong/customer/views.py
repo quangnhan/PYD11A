7da840401f6eac0817578dccbb292fa7f14e512d
Thong Tran <thongrun2s33@gmail.com>
2022-01-05 20:56:09 +0700

    add more codes

--- Day16/thong/customer/views.py
+++ Day16/thong/customer/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Customer
 
 # Create your views here.
+def customer_list(request):
+    list_customer = Customer.objects.all()
+    data = {"list_customer": list_customer}
+    return render(request, "customers/customer_list.html", data)
+
+
+def customer_create(request):
+    name = request.POST.get("name")
+    age = request.POST.get("age")
+
+    Customer.objects.create(name=name, age=age)
+    return HttpResponse(True)
