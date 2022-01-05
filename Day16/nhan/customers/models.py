b3c848327e6cadbbe262260e5799201c3a6d5b1e
nathan <quangnhan145@gmail.com>
2022-01-05 19:48:19 +0700

    Add model car

--- Day16/nhan/customers/models.py
+++ Day16/nhan/customers/models.py
@@ -4,4 +4,14 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
