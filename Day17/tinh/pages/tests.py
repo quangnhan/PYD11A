44ab0da4844bc2eb0c6b45540b1705ffc9026312
TongDangTinh <tinhtongdang@gmail.com>
2022-01-10 20:52:19 +0700

    Test case

--- Day17/tinh/pages/tests.py
+++ Day17/tinh/pages/tests.py
@@ -1,3 +1,15 @@
+
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    def test_home_page(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+class AboutPageTest(TestCase):
+    def test_about_page(self):
+        url = reverse("about")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
--- Day17/tinh/pages/urls.py
+++ Day17/tinh/pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePageView, AboutPageView
 
 urlpatterns = [ 
-    path('', HomePageView.as_view()),
-    path('about/', AboutPageView.as_view())
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about')
 ]
 
