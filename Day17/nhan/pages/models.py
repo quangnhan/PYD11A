5cd396d6de9eb1e4f32bb2f6a28c005ff908156a
nathan <quangnhan145@gmail.com>
2022-01-10 20:38:03 +0700

    Add testcase

--- Day17/nhan/pages/models.py
+++ Day17/nhan/pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+BLOG_STATUS_CHOICES = (
+    ("draft", "Draft"),
+    ("published", "Published"),
+)
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.CharField(choices=BLOG_STATUS_CHOICES, default="draft", max_length=10)
+    description = models.CharField(max_length=100, default="")
--- Day17/nhan/pages/tests.py
+++ Day17/nhan/pages/tests.py
@@ -1,3 +1,25 @@
+from django.urls import reverse
 from django.test import TestCase
+from .models import Blog
 
 # Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+class AboutPageTest(TestCase):
+    def test_about_page(self):
+        url = reverse("about")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+class BlogModelTest(TestCase):
+    def test_create_blog(self):
+        blog = Blog.objects.create(name="Blog 1")
+        self.assertEqual(blog.name, "Blog 1")
+        self.assertEqual(blog.status, "draft")
+
+
