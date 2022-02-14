64c4a8451962a0e9a6b0913852920ec9dbfa0011
nathan <quangnhan145@gmail.com>
2022-02-14 20:30:54 +0700

    add password reset

--- Day18/nhan/cafef/settings.py
+++ Day18/nhan/cafef/settings.py
@@ -129,3 +129,11 @@ LOGIN_REDIRECT_URL = 'blog_list'
 LOGOUT_REDIRECT_URL = 'login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'quangnhan145@gmail.com'
+EMAIL_HOST_PASSWORD = 'uogmlszfcwxblrlu'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
