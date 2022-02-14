5ae34475601976280088fb37267deb6e0724e8c8
nathan <quangnhan145@gmail.com>
2022-02-14 20:58:56 +0700

    Add login require mixin

--- Day18/nhan/blogs/views.py
+++ Day18/nhan/blogs/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Blog
-
 # Create your views here.
 
 class BlogListView(ListView):
@@ -10,7 +10,7 @@ class BlogListView(ListView):
     template_name = "blogs/blog_list.html"
     context_object_name = "list_blog"
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Blog
     template_name = "blogs/blog_create.html"
     fields = ('name', 'content', 'status', 'author')
@@ -23,7 +23,7 @@ class BlogDetailView(DetailView):
     template_name = "blogs/blog_detail.html"
     context_object_name = "blog"
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Blog
     template_name = "blogs/blog_update.html"
     fields = ('name', 'content', 'status', 'author')
@@ -32,7 +32,7 @@ class BlogUpdateView(UpdateView):
         blog = self.get_object()
         return reverse_lazy('blog_detail', args=(blog.id,))
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model = Blog
     template_name = "blogs/blog_delete.html"
     context_object_name = "blog"
